c14ca1e1cc63e4f2e239493489f3a020b7d772ae
RavinderSinghRawat <rawatravinder482002@gmail.com>
2024-10-23 07:38:13 +0530

    links added

--- controllers/ResetPassword.js
+++ controllers/ResetPassword.js
@@ -33,7 +33,7 @@ exports.resetPasswordToken = async (req, res) => {
     );
 
     //create url
-    const url = `http://localhost:3000/update-password/${token}`;
+    const url = `https://studynotion-ravinfinity.vercel.app/update-password/${token}`;
 
     //send mail containing the url
     await mailSender(
